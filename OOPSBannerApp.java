d48c598b5aef1893ddf732ded2bb5789b2354677
tp1219-cloud <tp1219@srmist.edu.in>
2026-02-24 09:20:21 +0530

    Coded UC2 to Print OOPS in Banner Format

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,2 +1,50 @@
-OOPSBannerApp.java
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String space = "   ";
+
+        // Line 1
+        System.out.println("  *****  " + space +
+                           "  *****  " + space +
+                           " ******  " + space +
+                           " *****   ");
+
+        // Line 2
+        System.out.println(" *     * " + space +
+                           " *     * " + space +
+                           " *     * " + space +
+                           " *       ");
+
+        // Line 3
+        System.out.println(" *     * " + space +
+                           " *     * " + space +
+                           " *     * " + space +
+                           " *       ");
+
+        // Line 4
+        System.out.println(" *     * " + space +
+                           " *     * " + space +
+                           " ******  " + space +
+                           "  ****   ");
+
+        // Line 5
+        System.out.println(" *     * " + space +
+                           " *     * " + space +
+                           " *       " + space +
+                           "      *  ");
+
+        // Line 6
+        System.out.println(" *     * " + space +
+                           " *     * " + space +
+                           " *       " + space +
+                           "      *  ");
+
+        // Line 7
+        System.out.println("  *****  " + space +
+                           "  *****  " + space +
+                           " *       " + space +
+                           " *****   ");
+    }
+}
 
