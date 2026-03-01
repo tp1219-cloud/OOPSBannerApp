8f0640b807d651a8d2893167faf7bac2ca63e937
tp1219-cloud <tp1219@srmist.edu.in>
2026-03-01 21:38:10 +0530

    UC3: Render OOPS banner using String.join

---
+++ UC3PrintOOPS.java
@@ -0,0 +1,45 @@
+public class UC3PrintOOPS {
+
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println(String.join("",
+                " *****   ",
+                " *****   ",
+                " *****   ",
+                " *****   "
+        ));
+
+        // Line 2
+        System.out.println(String.join("",
+                "*     *  ",
+                "*     *  ",
+                "*     *  ",
+                "*        "
+        ));
+
+        // Line 3
+        System.out.println(String.join("",
+                "*     *  ",
+                "*     *  ",
+                " *****   ",
+                " *****   "
+        ));
+
+        // Line 4
+        System.out.println(String.join("",
+                "*     *  ",
+                "*     *  ",
+                "*        ",
+                "      *  "
+        ));
+
+        // Line 5
+        System.out.println(String.join("",
+                " *****   ",
+                " *****   ",
+                "*        ",
+                " *****   "
+        ));
+    }
+}
