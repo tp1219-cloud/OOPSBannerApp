4e83631fbcd1e3e38ef641a2e793ea5912ed950c
tp1219-cloud <tp1219@srmist.edu.in>
2026-03-07 20:24:20 +0530

    Add UC5 OOPS Banner using Inline Array Initialization

--- UC5OOPSBannerApp.java
+++ UC5OOPSBannerApp.java
@@ -1,19 +1,45 @@
-public class UC5OOPSBannerApp {
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS Banner App");
+    }
+}
+public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join("   ", "   ***   ", "   ***   ", " ****** ", "   ***** "),
-            String.join("   ", " **   ** ", " **   ** ", " **   ** ", " **    "),
-            String.join("   ", "**     **", "**     **", " **   ** ", "**    "),
-            String.join("   ", "**     **", "**     **", " ******  ", "  ***  "),
-            String.join("   ", "**     **", "**     **", " **      ", "     **"),
-            String.join("   ", " **   ** ", " **   ** ", " **    ", "      **"),
-            String.join("   ", "   ***   ", "   ***   ", " **   ", "   ***** ")
-        };
+        String space = "   ";
 
-        for (String line : banner) {
-            System.out.println(line);
-        }
+        // Line 1
+        System.out.println("  *****  " + space +
+                           "  *****  " + space +
+                           " ******  " + space +
+                           " *****   ");
+
+        // Line 2
+        System.out.println(" *     * " + space +
+                           " *     * " + space +
+                           " *     * " + space +
+                           " *       ");
+
+        // Line 3
+        System.out.println(" *     * " + space +
+                           " *     * " + space +
+                           " *     * " + space +
+                           " *       ");
+
+        // Line 4
+        System.out.println(" *     * " + space +
+                           " *     * " + space +
+                           " ******  " + space +
+                           "  ****   ");
+
+        // Line 5
+        System.out.println(" *     * " + space +
+                           " *     
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS Banner App");
     }
-}
+}cd "/Users/harikrishna/OOPSBannerApp/" && javac 
+UC3PrintOOPS.java && java UC3PrintOOPS
+
